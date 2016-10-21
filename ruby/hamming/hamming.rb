073d3fe128828577db5c79487f01869e1b54f93b
Jaime Alvarez <jaime_andres@unitec.edu>
2016-10-21 03:28:35 +0000

    Hamming problem refactoring

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -4,8 +4,7 @@ end
 
 class Hamming
   def self.compute(strand1, strand2)
-    return 0 if strand1.empty? || strand2.empty?
-    raise(ArgumentError, "Strand lengths must be equal.") unless strand1.length == strand2.length
+    raise(ArgumentError, "Strand lengths are not equal -- Strand 1: #{strand1.length}, Strand 2: #{strand2.length}") unless strand1.length == strand2.length
     
     strand1.chars.each_with_index.count { |i, index| i != strand2[index] }
   end
