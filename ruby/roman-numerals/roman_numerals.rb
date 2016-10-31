c0814d86338143fa025f754e982b7866dae5f669
Jaime Alvarez <jaime_andres@unitec.edu>
2016-10-31 09:21:37 +0000

    Roman Numerals refactoring

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,21 +1,30 @@
-class Fixnum
+module Roman
+
+  ROMAN_NUMERALS = {
+      1000 => 'M', 900 => 'CM', 500 => 'D',
+      400 => 'CD', 100 => 'C', 90 => 'XC',
+      50 => 'L', 40 => 'XL', 10 => 'X',
+      9 => 'IX', 5 => 'V', 4 => 'IV',
+      1 => 'I' }
 
   def to_roman
-    roman_numerals = { 1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C', 90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I' }
-
     n = self
     roman_numeral = ''
 
-    roman_numerals.each do |key, value|
-      while n >= key do
-        roman_numeral << value
-        n -= key
+    ROMAN_NUMERALS.each do |integer, roman_number|
+      while n >= integer do
+        roman_numeral << roman_number
+        n -= integer
       end
     end
 
     roman_numeral
   end
 
+end
+
+class Integer
+  include Roman
 end
 
 module BookKeeping
