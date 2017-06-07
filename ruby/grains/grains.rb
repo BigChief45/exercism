0a4f60836a9b26b7abd16f3485357585d2dc2f8b
Jaime Alvarez <jaime_andres@unitec.edu>
2017-06-07 05:33:23 +0000

    refactor

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,11 +1,12 @@
 module Grains
-
   CHESSBOARD = (1..64)
 
   def self.square(square_number)
-    raise ArgumentError, 'The input must be a number between 1 and 64' unless CHESSBOARD.include?(square_number)
+    unless CHESSBOARD.include?(square_number)
+      raise ArgumentError, 'The input must be a number between 1 and 64'
+    end
 
-    2**(square_number-1)
+    2**(square_number - 1)
   end
 
   def self.total
